d8d9b9224ae9f8a9d81b8c6db4b7e039ec579f26
Adam <Adam@Adam-Komputer>
2014-06-15 20:15:59 +0200

    NullPointer

---
+++ EnterpriseApplication1-war/src/java/jsf/PasswordView.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jsf;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Adam
+ */
+@Named(value = "passwordView")
+@Dependent
+public class PasswordView {
+
+    /**
+     * Creates a new instance of PasswordView
+     */
+    private String login; 
+    private String password2;
+
+    public UserController getUserController() {
+        return userController;
+    }
+
+    public void setUserController(UserController userController) {
+        this.userController = userController;
+    }
+    private UserController userController;
+    
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
+    public void submit(){
+        this.getUserController().setuName(login);
+        this.getUserController().setuPassword(password2);
+        System.out.println(this.getUserController().checkUser());
+    }
+    public PasswordView() {
+    }
+    
+}
